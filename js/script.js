42c43e89fb230cbd904cbbd046c0639d6eb4e458
nilva2020 <piresnilva@gmail.com>
2025-01-03 02:30:55 -0300

    upgrade project

--- js/script.js
+++ js/script.js
@@ -1,5 +1,25 @@
-document
-  .querySelector(".hamburguer")
-  .addEventListener("click", () =>
-    document.querySelector(".container").classList.toggle("show-menu")
-  );
+document.addEventListener("DOMContentLoaded", function () {
+  const menuHamburguer = document.getElementById("menu-hamburguer");
+  const menu = document.getElementById("menu");
+  const fecharMenu = document.getElementById("fechar-menu");
+  const btnSaibaMais = document.getElementById("btn-saiba-mais");
+
+  if (menuHamburguer) {
+    menuHamburguer.addEventListener("click", function () {
+      menu.style.display = "flex";
+    });
+  }
+
+  if (fecharMenu) {
+    fecharMenu.addEventListener("click", function () {
+      menu.style.display = "none";
+    });
+  }
+
+  if (btnSaibaMais) {
+    btnSaibaMais.addEventListener("click", function (event) {
+      event.preventDefault();
+      menu.style.display = "flex";
+    });
+  }
+});
